5a5d7bed9f56da0295dde66e178ad859cabbf760
Tak H Kim <takhyun818@gmail.com>
2018-10-31 16:39:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(x)
-  if (2..x/2).none? do |i|
-    x % i == 0
-    return true
-  elsif x <= 1
-    return false
-  end
-end
+  if x <= 1
+		return false
+	else
+		(2..x/2).none? do |i| 
+			x % i == 0
+		end
+	end
 end
